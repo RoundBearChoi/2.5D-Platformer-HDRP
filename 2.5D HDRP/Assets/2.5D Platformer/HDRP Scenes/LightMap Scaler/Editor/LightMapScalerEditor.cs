3522f5c2afabb842ebfdf06e2060b01514d0149b
RoundBearChoi <roundbeargames@gmail.com>
2020-07-10 01:35:28 +0900

    Devlog#7 - added global lightmap scaler, fine tuned

---
+++ 2.5D HDRP/Assets/2.5D Platformer/HDRP Scenes/LightMap Scaler/Editor/LightMapScalerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Roundbeargames
+{
+    [CustomEditor(typeof(LightMapScaler))]
+    public class LightMapScalerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            LightMapScaler scaler = (LightMapScaler)target;
+
+            if (GUILayout.Button("Scale All Renderers"))
+            {
+                scaler.ScaleAll();
+            }
+        }
+    }
+}
+
---
+++ 2.5D HDRP/Assets/2.5D Platformer/HDRP Scenes/LightMap Scaler/LightMapScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public class LightMapScaler : MonoBehaviour
+    {
+        public float Scale;
+
+        public void ScaleAll()
+        {
+            Debug.Log("---Changing ALL LightMap Scale---");
+
+            MeshRenderer[] arr = FindObjectsOfType<MeshRenderer>();
+
+            foreach(MeshRenderer r in arr)
+            {
+                Debug.Log(r.gameObject.name);
+                r.scaleInLightmap = Scale;
+            }
+        }
+    }
+}
