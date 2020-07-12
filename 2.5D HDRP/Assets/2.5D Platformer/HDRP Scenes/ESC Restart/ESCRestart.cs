9afa7c3c055d1d4e3e583fb8b2d0ca9cadcd30ef
RoundBearChoi <roundbeargames@gmail.com>
2020-07-12 13:02:27 +0900

    Devlog#7 - testing probuilder export

---
+++ 2.5D HDRP/Assets/2.5D Platformer/HDRP Scenes/ESC Restart/ESCRestart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Roundbeargames
+{
+    public class ESCRestart : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
