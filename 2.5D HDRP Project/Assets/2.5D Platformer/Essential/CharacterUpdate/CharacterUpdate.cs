7e087e7c01a4101acbf5ab4ed8010437de1834e0
c <roundbeargames@gmail.com>
2020-07-20 17:29:34 +0900

    Devlog#8 - character update query, characterupdatetype enum separate

--- 2.5D HDRP Project/Assets/2.5D Platformer/Essential/CharacterUpdate/CharacterUpdate.cs
+++ 2.5D HDRP Project/Assets/2.5D Platformer/Essential/CharacterUpdate/CharacterUpdate.cs
@@ -4,24 +4,6 @@ using UnityEngine;
 
 namespace Roundbeargames
 {
-    public enum CharacterUpdateType
-    {
-        MANUALINPUT,
-        LEDGECHECKER,
-        RAGDOLL,
-        BLOCKINGOBJECTS,
-        BOX_COLLIDER_UPDATER,
-        VERTICAL_VELOCITY,
-        DAMAGE_DETECTOR,
-        COLLISION_SPHERES,
-        INSTA_KILL,
-        PLAYER_ATTACK,
-        PLAYER_ANIMATION,
-        PLAYER_ROTATION,
-
-        COUNT,
-    }
-
     public abstract class CharacterUpdate : MonoBehaviour
     {
         protected CharacterUpdateProcessor characterUpdateProcessor;
---
+++ 2.5D HDRP Project/Assets/2.5D Platformer/Essential/CharacterUpdate/CharacterUpdateType.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Roundbeargames
+{
+    public enum CharacterUpdateType
+    {
+        MANUALINPUT,
+        LEDGECHECKER,
+        RAGDOLL,
+        BLOCKINGOBJECTS,
+        BOX_COLLIDER_UPDATER,
+        VERTICAL_VELOCITY,
+        DAMAGE_DETECTOR,
+        COLLISION_SPHERES,
+        INSTA_KILL,
+        PLAYER_ATTACK,
+        PLAYER_ANIMATION,
+        PLAYER_ROTATION,
+
+        COUNT,
+    }
+}
