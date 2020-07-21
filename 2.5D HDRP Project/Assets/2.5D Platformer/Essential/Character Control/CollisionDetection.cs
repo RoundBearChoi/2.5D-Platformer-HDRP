76fb9951d48a1de8ddfddaba135f0af3e1ef51b9
RoundBearChoi <roundbeargames@gmail.com>
2020-07-22 01:09:21 +0900

    Devlog#8 - unnecessary lines

--- 2.5D HDRP Project/Assets/2.5D Platformer/Essential/Character Control/CollisionDetection.cs
+++ 2.5D HDRP Project/Assets/2.5D Platformer/Essential/Character Control/CollisionDetection.cs
@@ -20,40 +20,6 @@ namespace Roundbeargames
             return false;
         }
 
-        public static GameObject GetCollidingObject(CharacterControl control, GameObject start, Vector3 dir,
-            float blockDistance, ref Vector3 collisionPoint)
-        {
-            collisionPoint = Vector3.zero;
-
-            //Draw debug line
-            Debug.DrawRay(start.transform.position, dir * blockDistance, Color.yellow);
-
-            //Check collision
-            RaycastHit hit;
-            if (Physics.Raycast(start.transform.position, dir,
-                out hit,
-                blockDistance))
-            {
-                if (!IsBodyPart(control, hit.collider) &&
-                    !IsIgnoringCharacter(control, hit.collider) &&
-                    !Ledge.IsLedgeChecker(hit.collider.gameObject) &&
-                    !MeleeWeapon.IsWeapon(hit.collider.gameObject) &&
-                    !TrapSpikes.IsTrap(hit.collider.gameObject))
-                {
-                    collisionPoint = hit.point;
-                    return hit.collider.transform.root.gameObject;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            else
-            {
-                return null;
-            }
-        }
-
         static bool IsIgnoringCharacter(CharacterControl control, Collider col)
         {
             if (!control.ANIMATION_DATA.IsIgnoreCharacterTime)
